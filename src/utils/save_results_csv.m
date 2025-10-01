0a4428d5c76fff7002af709dd68a4899cbd87e41
DK-PandaraBoy <panda8dk.de@gmail.com>
2025-10-01 12:26:24 +0200

    Update save_results_csv.m

--- src/utils/save_results_csv.m
+++ src/utils/save_results_csv.m
@@ -1,1 +1,8 @@
-
+function save_results_csv(mat, outpath)
+% mat columns: OSR, bits, order, SNR_dB, ENOB
+T = array2table(mat, 'VariableNames', {'OSR','Bits','Order','SNR_dB','ENOB'});
+if ~exist(fileparts(outpath),'dir')
+mkdir(fileparts(outpath));
+end
+writetable(T, outpath);
+end
