e4eb9a1f02be7a92a6d524179e932c3f0bea959d
DK-PandaraBoy <panda8dk.de@gmail.com>
2025-10-01 12:18:30 +0200

    Update cm_integrator.m

--- src/blocks/cm_integrator.m
+++ src/blocks/cm_integrator.m
@@ -1,1 +1,7 @@
-
+function block = cm_integrator(name, gain)
+% cm_integrator - return a struct describing a current-mode integrator
+if nargin < 2, gain = 1; end
+block.type = 'integrator';
+block.name = name;
+block.gain = gain;
+end
